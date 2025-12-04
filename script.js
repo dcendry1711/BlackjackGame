dd5f36826b75b31cac1e04e4fd1e5ae03fc2d767
dcendry1711 <daniel.cendry@gmail.com>
2025-12-04 19:30:44 +0100

    basic style of game

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const secondCard = Math.floor((Math.random() * 11)+1)
 const sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
+let message = ''
 
 console.log(firstCard)
 console.log(secondCard)
 console.log(sum)
 
 if(sum === 21){
-    console.log('Blackjack!')
+    message = 'Blackjack!'
     hasBlackjack = true
 } else if(sum <= 21){
-    console.log('Do you want draw another card?')
+    message = 'Do you want draw another card?'
 } else {
-    console.log('You lose!!!')
+    message = 'You lose!!!'
     isAlive = false
 }
