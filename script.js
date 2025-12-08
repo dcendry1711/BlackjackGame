28dc697f1316a6fa6691e922e5d595f63d160fa3
dcendry1711 <daniel.cendry@gmail.com>
2025-12-08 17:35:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -51,8 +51,6 @@ function renderGame(){
         message = 'Do you want draw another card?'
         startBtn.style.visibility = 'hidden'
         nextCardBtn.style.display = 'block'
-        nextCardBtn.style.position = 'relative'
-        nextCardBtn.style.top = '-132px'
     } else {
         message = 'You lose!!!'
         nextCardBtn.style.display = 'none'
